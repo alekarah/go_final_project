e7911abae9a749403eef34026fa6ae30f6f8c03b
Alexandra Saganelidze <alekarah.all@gmail.com>
2025-06-23 20:20:04 +0300

    create a web server

---
+++ pkg/server/server.go
@@ -0,0 +1,30 @@
+package server
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+const (
+	defaultPort = "7540"
+	webDir      = "./web"
+)
+
+// StartServer запускает веб-сервер
+func StartServer() {
+	// Получаем порт из переменной окружения или используем по умолчанию
+	port := os.Getenv("TODO_PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	// Настраиваем файл-сервер для обслуживания статических файлов
+	http.Handle("/", http.FileServer(http.Dir(webDir)))
+
+	// Запускаем сервер
+	fmt.Printf("Сервер запущен на порту %s\n", port)
+	fmt.Printf("Веб-интерфейс: http://localhost:%s\n", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
