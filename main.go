3ab302af2cf0aaa1ca2383f049acb7c4fd9aafd8
Alexandra Saganelidze <alekarah.all@gmail.com>
2025-06-23 20:36:39 +0300

    add SQLite database

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "todo/pkg/server"
+import (
+	"log"
+	"os"
+
+	"todo/pkg/db"
+	"todo/pkg/server"
+)
 
 func main() {
+	// Определяем путь к файлу базы данных
+	dbFile := os.Getenv("TODO_DBFILE")
+	if dbFile == "" {
+		dbFile = "scheduler.db"
+	}
+
+	// Инициализируем базу данных
+	err := db.Init(dbFile)
+	if err != nil {
+		log.Fatal("Ошибка инициализации базы данных:", err)
+	}
+
+	// Запускаем сервер
 	server.StartServer()
 }
